bc2c41dd76f596f28ad56142be7f644ab6f3ef2b
Jaden <agent.jaden2@gmail.com>
2018-03-24 12:51:02 +0900

    Update script file

--- update_div.py
+++ update_div.py
@@ -10,6 +10,98 @@ import urllib.request
 import pickle
 import getopt
 import sys
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+def write_google_spreadsheet(num_stock, stock_cat_list, stock_name_list, stock_num_list, stock_url_list, stock_dps_list):
+
+	# Write_google_spreadsheet
+	# use creds to create a client to interact with the Google Drive API
+	scope = ['https://spreadsheets.google.com/feeds',
+			'https://www.googleapis.com/auth/drive']
+	creds = ServiceAccountCredentials.from_json_keyfile_name('mykey.json', scope)
+	client = gspread.authorize(creds)
+	 
+	# Find a workbook by name and open the first sheet
+	# Make sure you use the right name here.
+	sheet = client.open("test_python").sheet1
+	print(type(sheet))
+
+	sheet.update_cell(1, 1, "Category")
+	sheet.update_cell(1, 2, "Name")
+	sheet.update_cell(1, 3, "Code")
+	sheet.update_cell(1, 4, "code for google")
+	sheet.update_cell(1, 5, "URL")
+	sheet.update_cell(1, 6,  "2006.12")
+	sheet.update_cell(1, 7,  "2007.12")
+	sheet.update_cell(1, 8, "2008.12")
+	sheet.update_cell(1, 9, "2009.12")
+	sheet.update_cell(1, 10, "2010.12")
+	sheet.update_cell(1, 11, "2011.12")
+	sheet.update_cell(1, 12, "2012.12")
+	sheet.update_cell(1, 13, "2013.12")
+	sheet.update_cell(1, 14, "2014.12")
+	sheet.update_cell(1, 15, "2015.12")
+	sheet.update_cell(1, 16, "2016.12")
+	sheet.update_cell(1, 17, "2017.12")
+	sheet.update_cell(1, 18, "시가")
+	sheet.update_cell(1, 19, "시가배당률(2016)")
+	sheet.update_cell(1, 20, "시가배당률(2017)")
+
+	cell_list = sheet.range(2,1, 1+num_stock, 1)
+	for k, cell in enumerate(cell_list):
+		cell.value = stock_cat_list[k]
+	sheet.update_cells(cell_list)
+
+	cell_list = sheet.range(2,2, 1+num_stock, 2)
+	for k, cell in enumerate(cell_list):
+		cell.value = stock_name_list[k]
+	sheet.update_cells(cell_list)
+
+	cell_list = sheet.range(2,3, 1+num_stock, 3)
+	for k, cell in enumerate(cell_list):
+		cell.value = stock_num_list[k]
+	sheet.update_cells(cell_list)
+
+	cell_list = sheet.range(2,4, 1+num_stock, 4)
+	for k, cell in enumerate(cell_list):
+		col = str(k+2)
+		formula = '=if(A'+col+'="코스닥", "KOSDAQ:"&text(C'+col+',"000000"), if(A'+col+'="코스넥", "N/A","KRX:"&text(C'+col+',"000000")))'
+		cell.value = formula
+	sheet.update_cells(cell_list, value_input_option='USER_ENTERED')
+
+	cell_list = sheet.range(2,5, 1+num_stock, 5)
+	for k, cell in enumerate(cell_list):
+		cell.value = stock_url_list[k]
+	sheet.update_cells(cell_list)
+
+	for l in range(len(stock_dps_list[0])-1):
+		print(l)
+		cell_list = sheet.range(2, 6+l, 1+num_stock, 6+l)
+		for k, cell in enumerate(cell_list):
+			cell.value = stock_dps_list[k][l]
+		sheet.update_cells(cell_list)
+
+	cell_list = sheet.range(2, 18, 1+num_stock, 18)
+	for k, cell in enumerate(cell_list):
+		col = str(k+2)
+		formula = '=iferror(googlefinance(D'+col+'), 0)'
+		cell.value = formula
+	sheet.update_cells(cell_list, value_input_option='USER_ENTERED')
+
+	cell_list = sheet.range(2, 19, 1+num_stock, 19)
+	for k, cell in enumerate(cell_list):
+		col = str(k+2)
+		formula = '=if(OR(P'+col+'="N/A", R'+col+'=0),0, P'+col+'/R'+col+')'
+		cell.value = formula
+	sheet.update_cells(cell_list, value_input_option='USER_ENTERED')
+
+	cell_list = sheet.range(2, 20, 1+num_stock, 20)
+	for k, cell in enumerate(cell_list):
+		col = str(k+2)
+		formula = '=if(OR(Q'+col+'="N/A", R'+col+'=0),0, Q'+col+'/R'+col+')'
+		cell.value = formula
+	sheet.update_cells(cell_list, value_input_option='USER_ENTERED')
 
 def main():
 
@@ -28,7 +120,7 @@ def main():
 			sys.exit(2)
 	
 	### PART 1 - Read DPS Excel file
-	num_stock = 2040
+	num_stock = 2046
 	input_file = "div_crawling_result.xlsx"
 	cur_dir = os.getcwd()
 	workbook_name = input_file
@@ -190,6 +282,8 @@ def main():
 			worksheet_dps.write(1+k, 4+l, stock_dps_list[k][l])
 
 	workbook.close()
+
+	write_google_spreadsheet(num_stock, stock_cat_list, stock_name_list, stock_num_list, stock_url_list, stock_dps_list)
 
 # Main
 if __name__ == "__main__":
